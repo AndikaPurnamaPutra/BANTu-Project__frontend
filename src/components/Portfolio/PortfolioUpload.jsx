b04fd61df8b98b543b26cdae17a1e13792f5175b
Ucok <ucok@Ucoks-MacBook-Pro.local>
2025-05-31 17:24:12 +0700

    first commit

--- src/components/Portfolio/PortfolioUpload.jsx
+++ src/components/Portfolio/PortfolioUpload.jsx
@@ -63,6 +63,7 @@ const PortfolioUpload = () => {
     content: description,
     onUpdate: ({ editor }) => {
       setDescription(editor.getHTML());
+      checkFormValidity(title, html, selectedCategory, mainFile);
     },
   });
 
